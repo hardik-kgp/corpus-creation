5d4cb10bd0898971bd888b64737cf9a91984d30c
hardik-kgp <hardik8464@iitkgp.ac.in>
2019-12-26 02:49:28 +0530

    last commit

--- Codes/stopwords.py
+++
@@ -1,7 +0,0 @@
-import pickle
-with open('stopwords.txt', 'r+') as f:
-    stopwords = [line.strip() for line in f]
-stopwords = set(stopwords)
-
-with open('stopwords.pckl', 'wb') as f:
-	pickle.dump(stopwords, f)
